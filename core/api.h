bdaef8cce1bad0ba1a192e74bfe6a26653fa0960
smarsu <754247874@qq.com>
2020-04-07 19:31:32 +0800

    c

--- core/api.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2020 smarsu. All Rights Reserved.
-
-#pragma once
-#include "core/ast.h"
-#include "core/codegen.h"
-#include "core/logging.h"
-
-namespace smcc {
-}  // namespace smcc
--- core/logging.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2020 smarsu. All Rights Reserved.
-
-#pragma once
-#include <iostream>
-
-#include "glog/logging.h"
-
-namespace smcc {
-}  // namespace smcc
